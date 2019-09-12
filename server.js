06f008d8b697680c19d36d8daab24d6b2d04a3d9
Mass <masval444@msn.com>
2019-09-12 21:56:29 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'mass',
+    password : '',
+    database : 'facefinder'
+  }
+}); 
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send('We are live:D') })
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, salt) });
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
