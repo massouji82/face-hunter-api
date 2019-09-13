6f35641d55c668642e75d7eea69a48f8a90261f6
Mass <masval444@msn.com>
2019-09-13 11:50:19 +0200

    add db connection

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-silhouetted-38716',
-    user : 'mass',
-    password : '',
-    database : 'facefinder'
+    host : process.env.DATABASE_URL,
+    ssl : true
   }
 }); 
 
